f17d2521cde7a5a4f256fec365cfc7cabc53f1f4
Chriss Desmonds <chimbadzwa.christopherdesmond@gmail.com>
2022-08-11 11:52:27 +0200

    database file

--- paymentpro.sql
+++ paymentpro.sql
@@ -16,6 +16,127 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `fees`
+--
+
+DROP TABLE IF EXISTS `fees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `year` varchar(45) DEFAULT NULL,
+  `period` int DEFAULT NULL,
+  `levy` float NOT NULL,
+  `tuition` float NOT NULL,
+  `total` float NOT NULL,
+  `minimum` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fees`
+--
+
+LOCK TABLES `fees` WRITE;
+/*!40000 ALTER TABLE `fees` DISABLE KEYS */;
+INSERT INTO `fees` VALUES (1,'2022',1,200,50,250,150),(2,'2022',2,200,50,250,175),(3,'2022',3,250,75,325,225);
+/*!40000 ALTER TABLE `fees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `student` int NOT NULL,
+  `fee` int NOT NULL,
+  `date_modified` varchar(45) DEFAULT NULL,
+  `amount` float NOT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payments`
+--
+
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+INSERT INTO `payments` VALUES (1,1,1,'2022-08-10',175,'owing'),(2,4,1,'2022-08-10',250,'paid');
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fname` varchar(45) DEFAULT NULL,
+  `lname` varchar(45) DEFAULT NULL,
+  `status` varchar(45) NOT NULL DEFAULT 'active',
+  `reg_number` varchar(45) NOT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `dob` varchar(45) DEFAULT NULL,
+  `address` varchar(95) DEFAULT NULL,
+  `pname` varchar(75) DEFAULT NULL,
+  `pmobile` varchar(45) DEFAULT NULL,
+  `pemail` varchar(45) DEFAULT NULL,
+  `year` varchar(45) NOT NULL DEFAULT '2022',
+  `period` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `reg_number_UNIQUE` (`reg_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Kimberly','Mutasa','active','63-1499000J50','0734000000','Female','2010-01-05','6A Nursery Hill,\r\nNumber 1,\r\nHwange','Chriss desmonds','0770000000','chris@luminsoft.com','2022',1),(4,'Lashon','James','active','63-1499001K90','0734000019','Female','2009-06-06','3614 Hatcliffe Ext,\r\nBorrowdale\r\nHarare','Ngoni Mutasa','0770000000','ngoni@gmail.com','2022',2);
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `student` int NOT NULL,
+  `fee` int NOT NULL,
+  `date` varchar(45) DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,1,1,'2022-08-10',175),(2,4,1,'2022-08-10',250);
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -38,7 +159,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +168,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'cdesy','Chriss','Desmonds','Male','chris@luminsoft.com','4000000','2021-11-23','Admin','qwerty','906907','active'),(3,'prim','Primrose','Mhlanga','Female','primrosemhlanga8@gmail.com','0770000000','2018-01-21','Admin','12345','906907','active');
+INSERT INTO `users` VALUES (5,'clerk','Test ','Clerk','Female','clerk@gmail.com','0770000000','2000-01-01','Clerk','1234','id-123','active'),(6,'headmaster','Test','Headmaster','Male','head@gmail.com','0780000000','1970-12-31','Head','1234','id456','active'),(7,'bursar','Test','Bursar','Female','bursar@gmail.com','0710000000','1985-06-01','Bursar','1234','id-789','active');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -60,4 +181,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-09 14:58:48
+-- Dump completed on 2022-08-11 11:50:07
