c567bbb39ee69dd3425d2f493d5e11eb3bbbb7d7
Chriss Desmonds <chimbadzwa.christopherdesmond@gmail.com>
2022-08-13 10:42:11 +0200

    owing student report

---
+++ admin/report3.php
@@ -0,0 +1,136 @@
+<?php
+
+    session_start();
+    include('./includes/dbcon.php');
+
+    if(!isset($_SESSION['username'])){
+        echo "<script type='text/javascript'> document.location ='./controllers/logout.php'; </script>";
+    }
+
+    //get employees
+    $sql = "SELECT
+                amount, fname, lname, f.year, f.period, total
+            FROM payments AS p
+            LEFT JOIN students AS s ON p.student = s.id
+            INNER JOIN fees AS f ON p.fee = f.id
+            WHERE p.status = 'owing'";
+    $statement = $db->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetchAll();
+
+    // echo $result;
+
+?>
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="../dist/img/AdminLTELogo.png"/>
+
+  <title>Payments | Payments</title>
+
+  <?php include('./includes/styles.php'); ?>
+  
+</head>
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        <?php include('./includes/header.php'); ?>
+
+        <!-- Main Sidebar Container -->
+        <?php include('./includes/sidebar.php'); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Reports</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Owing</a></li>
+                            <li class="breadcrumb-item active">All Owing</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-md-6"><h3 class="card-title">List of Owing Student</h3></div>
+                                <div class="col-md-6 text-right">
+                                    <!-- <button data-target="#add" data-toggle="modal" class="btn btn-sm btn-outline-primary">
+                                        <i class="fa fa-plus"></i>&nbsp;&nbsp;Add
+                                    </button> -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Period</th>
+                                            <th>Student</th>
+                                            <th>Amount</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($result as $r) {
+                                            $bal = $r['total'] - $r['amount']
+                                            ?>
+                                            <tr>
+                                                <td><?php echo $r['year']. ' / Term-'. $r['period']; ?></td>
+                                                <td><?php echo $r['fname']. ' '. $r['lname']; ?></td>
+                                                <td><?php echo 'US$'.$bal; ?></td>
+                                                
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+
+                </div><!--/. container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Main Footer -->
+        <?php include('./includes/footer.php'); ?>
+
+    </div>
+    <!-- ./wrapper -->  
+
+    <!-- REQUIRED SCRIPTS -->
+    <?php include('./includes/javascripts.php'); ?>
+
+    <script>
+
+    </script>
+    
+</body>
+</html>
